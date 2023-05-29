cdc88a04e8e8268cf3542292f81705c70ca0267b
Karolina Piech <karolinapiech1990@gmail.com>
2023-05-29 22:38:20 +0000

    Add second and third set of cards to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,152 @@
+// Creates the first card set
 let cardsArray1 = [];
 
-cardsArray1[0]  = {
+cardsArray1[0] = {
   image01: new Image(),
   src: "assets/images/advice.jpg",
   imageCaption: "Listen closely to your friends and colleagues today. You may get a very helpful advice!"
 };
 
-cardsArray1[1]  = {
-  image01: new Image(),
+cardsArray1[1] = {
+  image02: new Image(),
   src: "assets/images/angry.jpg",
   imageCaption: "You may feel quite angry today. No matter what caused you feel this way don't let it ruin your day!"
 };
 
-cardsArray1[2]  = {
-  image01: new Image(),
+cardsArray1[2] = {
+  image03: new Image(),
   src: "assets/images/annoyed.jpg",
   imageCaption: "Even a little things may get on your nerves today. Take a deep breaths and try not to overreact."
 };
 
-cardsArray1[3]  = {
-  image01: new Image(),
+cardsArray1[3] = {
+  image04: new Image(),
   src: "assets/images/anxiety.jpg",
   imageCaption: "Today your anxiety will try to take control over you. Don't let it win!"
 };
 
-cardsArray1[3]  = {
-  image01: new Image(),
+cardsArray1[3] = {
+  image05: new Image(),
   src: "assets/images/attack-chance.jpg",
   imageCaption: "Stay focused on your surroundings. Some unexpected situations may arrise!"
 };
 
-cardsArray1[4]  = {
-  image01: new Image(),
+cardsArray1[4] = {
+  image06: new Image(),
   src: "assets/images/attitude.jpg",
   imageCaption: "Today you should try to take responsibility for your emotions. Remember, that your attitude is created by your thoughts, and you CAN choose your thoughts."
 };
 
-cardsArray1[5]  = {
-  image01: new Image(),
+cardsArray1[5] = {
+  image07: new Image(),
   src: "assets/images/bad-day.jpg",
   imageCaption: "Maybe today is not going to be the one of your best days, but remember that you don't need to spread the negative vibrations to others."
 };
 
+// Prints the array of cards to the console
+
 for(x = 0; x < cardsArray1.length; x++) {
   console.log(cardsArray1[x].src);
+}
+
+// Creates the second card set
+let cardsArray2 = [];
+
+cardsArray2[0] = {
+  image08: new Image(),
+  src: "assets/images/bizarre-adventure.jpg",
+  imageCaption: "A high chance of a bizarre adventure. For example you may encounter some very fancy looking people having shady connections with Italian mafia... Or not."
+};
+
+cardsArray2[1] = {
+  image09: new Image(),
+  src: "assets/images/caffeine.jpg",
+  imageCaption: "Try to control your caffeine intake today. Your heart is going to thank you in the future!"
+};
+
+cardsArray2[2] = {
+  image10: new Image(),
+  src: "assets/images/calm.jpg",
+  imageCaption: "Even if things will go not exactly as planned today, you will find yourself calm and won't let anything to ruin your peace."
+};
+
+cardsArray2[3] = {
+  image11: new Image(),
+  src: "assets/images/doctor.jpg",
+  imageCaption: "You should focus on your health today. Remember, that you own only one body and you need to take a good care of it!"
+};
+
+cardsArray2[3] = {
+  image12: new Image(),
+  src: "assets/images/drama-time.jpg",
+  imageCaption: "You may try to hide it, but you surely like some spicy gossips (who doesn't?). Today is the day when your hunger for drama may be fulfilled!"
+};
+
+cardsArray2[4] = {
+  image13: new Image(),
+  src: "assets/images/foodie.jpg",
+  imageCaption: "Today you may expect some delicious food! No matter if it's either healthy snack, little salami or big dinner, it definitely will fill you up with happiness."
+};
+
+cardsArray2[5] = {
+  image13: new Image(),
+  src: "assets/images/fragile.jpg",
+  imageCaption: "You may feel very fragile- either physically, emotionally or mentally. Take it easy today, and don't be afraid to share your feelings with your close ones!"
+};
+
+// Prints the array of cards to the console
+
+for(x = 0; x < cardsArray2.length; x++) {
+  console.log(cardsArray2[x].src);
+}
+
+// Creates the third card set
+let cardsArray3 = [];
+
+cardsArray3[0] = {
+  image14: new Image(),
+  src: "assets/images/friend-hangout.jpg",
+  imageCaption: "Spend your time with your friends today. Even if it's going to be a short hangout it will brighten your day and you will have a great time together!"
+};
+
+cardsArray3[1] = {
+  image15: new Image(),
+  src: "assets/images/fully-charged.jpg",
+  imageCaption: "You are going to feel full of energy. This day is going to be full of positive vibes!"
+};
+
+cardsArray3[2] = {
+  image16: new Image(),
+  src: "assets/images/guests-over.jpg",
+  imageCaption: "You are going to be busy with guests today. If you didn't expect any- now you know that you may be surprised by unanounced visit!"
+};
+
+cardsArray3[3] = {
+  image17: new Image(),
+  src: "assets/images/job-opportunity.jpg",
+  imageCaption: "New job opportunity may arise within your area of interest! Be ready to update your CV as it may be your chance for a pay raise!"
+};
+
+cardsArray3[3] = {
+  image18: new Image(),
+  src: "assets/images/lazy-cosy.jpg",
+  imageCaption: "Today maybe you may not do much, but don't worry. Taking a day off from your tasks may help your brain to recover and become more productive tomorrow!"
+};
+
+cardsArray3[4] = {
+  image19: new Image(),
+  src: "assets/images/love-messenger.jpg",
+  imageCaption: "Love has many faces. Whichever of them you choose today, make sure to value and celebrate this feeling!"
+};
+
+cardsArray3[5] = {
+  image20: new Image(),
+  src: "assets/images/nice.jpg",
+  imageCaption: "Having THAT outfit on surely feels good. Today you will get an extra confidence boost thanks to that."
+};
+
+// Prints the array of cards to the console
+
+for(x = 0; x < cardsArray2.length; x++) {
+  console.log(cardsArray2[x].src);
 }
