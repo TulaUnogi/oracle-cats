855e17661ece20046fd2cb435a86a4cef3b6e468
Karolina Piech <karolinapiech1990@gmail.com>
2023-06-04 15:18:57 +0000

    Swap the card pictures for smaller size ones, revert to old deck building method and rewrite setRandomize function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ let contentBox = document.getElementById("content-box");
 
 // Adds event listener to the page 1 button
 let button1 = document.getElementById("button1");
-button1.addEventListener("click", pageTwo, buildDeck);
+button1.addEventListener("click", pageTwo);
 
 // On click function changing the content-box div's html for page 2 content
 
@@ -31,58 +31,142 @@ function pageTwo(event) {
   `;
 
   let button2 = document.getElementById("button2");
-  button2.addEventListener("click", pageThree, setRandomize); 
+  button2.addEventListener("click", pageThree); 
 }
 
-// New approach to build a deck of cards
-
+// Creates the deck
 let deck = [];
 
-function buildDeck() {
+deck[0] = {
+  image01: new Image(),
+  src: "assets/images/advice.jpg",
+  imageCaption: "Listen closely to your friends and colleagues today. You may get a very helpful advice!"
+};
 
-  let pictures = [
-    assets/images/advice.jpg, assets/images/angry.jpg, assets/images/annoyed.jpg, assets/images/anxiety.jpg, 
-    assets/images/attack-chance.jpg, assets/images/attitude.jpg, assets/images/bad-day.jpg, assets/images/bizarre-adventure.jpg,
-    assets/images/caffeine.jpg, assets/images/calm.jpg, assets/images/doctor.jpg, assets/images/drama-time.jpg, assets/images/foodie.jpg,
-    assets/images/fragile.jpg, assets/images/friend-hangout.jpg, assets/images/fully-charged.jpg, assets/images/guests-over.jpg,
-    assets/images/job-opportunity.jpg, assets/images/lazy-cosy.jpg, assets/images/love-messenger.jpg, assets/images/nice.jpg,
-  ];
+deck[1] = {
+  image02: new Image(),
+  src: "assets/images/angry.jpg",
+  imageCaption: "You may feel quite angry today. No matter what caused you feel this way don't let it ruin your day!"
+};
 
-  let descriptions = [
-    "Listen closely to your friends and colleagues today. You may get a very helpful advice!", 
-    "You may feel quite angry today. No matter what caused you feel this way don't let it ruin your day!", 
-    "Even a little things may get on your nerves today. Take a deep breaths and try not to overreact.",
-    "Today your anxiety will try to take control over you. Don't let it win!", "Stay focused on your surroundings. Some unexpected situations may arrise!",
-    "Today you should try to take responsibility for your emotions. Remember, that your attitude is created by your thoughts, and you CAN choose your thoughts.",
-    "Maybe today is not going to be the one of your best days, but remember that you don't need to spread the negative vibrations to others.",
-    "A high chance of a bizarre adventure. For example you may encounter some very fancy looking people having shady connections with Italian mafia... Or not.",
-    "Try to control your caffeine intake today. Your heart is going to thank you in the future!", "Even if things will go not exactly as planned today, you will find yourself calm and won't let anything to ruin your peace.",
-    "You should focus on your health today. Remember, that you own only one body and you need to take a good care of it!",
-    "You may try to hide it, but you surely like some spicy gossips (who doesn't?). Today is the day when your hunger for drama may be fulfilled!",
-    "Today you may expect some delicious food! No matter if it's either healthy snack, little salami or big dinner, it definitely will fill you up with happiness.",
-    "You may feel very fragile- either physically, emotionally or mentally. Take it easy today, and don't be afraid to share your feelings with your close ones!",
-    "Spend your time with your friends today. Even if it's going to be a short hangout it will brighten your day and you will have a great time together!",
-    "You are going to feel full of energy. This day is going to be full of positive vibes!", "You are going to be busy with guests today. If you didn't expect any- now you know that you may be surprised by unanounced visit!",
-    "New job opportunity may arise within your area of interest! Be ready to update your CV as it may be your chance for a pay raise!",
-    "Today maybe you may not do much, but don't worry. Taking a day off from your tasks may help your brain to recover and become more productive tomorrow!",
-    "Love has many faces. Whichever of them you choose today, make sure to value and celebrate this feeling!",
-    "Having THAT outfit on surely feels good. Today you will get an extra confidence boost thanks to that."
-  ];
+deck[2] = {
+  image03: new Image(),
+  src: "assets/images/annoyed.jpg",
+  imageCaption: "Even a little things may get on your nerves today. Take a deep breaths and try not to overreact."
+};
 
-  for (var z= 0; z < pictures.length; z++) {
-    for (var d = 0; d < description.length; d++) {
-      let picture = pictures[z];
-      let description = descriptions[d];
-      deck.push({picture, description});
-    }
-  }
-  return deck;
-}
+deck[3] = {
+  image04: new Image(),
+  src: "assets/images/anxiety.jpg",
+  imageCaption: "Today your anxiety will try to take control over you. Don't let it win!"
+};
+
+deck[4] = {
+  image05: new Image(),
+  src: "assets/images/attack-chance.jpg",
+  imageCaption: "Stay focused on your surroundings. Some unexpected situations may arrise!"
+};
+
+deck[5] = {
+  image06: new Image(),
+  src: "assets/images/attitude.jpg",
+  imageCaption: "Today you should try to take responsibility for your emotions. Remember, that your attitude is created by your thoughts, and you CAN choose your thoughts."
+};
+
+deck[6] = {
+  image07: new Image(),
+  src: "assets/images/bad-day.jpg",
+  imageCaption: "Maybe today is not going to be the one of your best days, but remember that you don't need to spread the negative vibrations to others."
+};
+
+deck[7] = {
+  image08: new Image(),
+  src: "assets/images/bizarre-adventure.jpg",
+  imageCaption: "A high chance of a bizarre adventure. For example you may encounter some very fancy looking people having shady connections with Italian mafia... Or not."
+};
+
+deck[8] = {
+  image09: new Image(),
+  src: "assets/images/caffeine.jpg",
+  imageCaption: "Try to control your caffeine intake today. Your heart is going to thank you in the future!"
+};
+
+deck[9] = {
+  image10: new Image(),
+  src: "assets/images/calm.jpg",
+  imageCaption: "Even if things will go not exactly as planned today, you will find yourself calm and won't let anything to ruin your peace."
+};
+
+deck[10] = {
+  image11: new Image(),
+  src: "assets/images/doctor.jpg",
+  imageCaption: "You should focus on your health today. Remember, that you own only one body and you need to take a good care of it!"
+};
+
+deck[11] = {
+  image12: new Image(),
+  src: "assets/images/drama-time.jpg",
+  imageCaption: "You may try to hide it, but you surely like some spicy gossips (who doesn't?). Today is the day when your hunger for drama may be fulfilled!"
+};
+
+deck[12] = {
+  image13: new Image(),
+  src: "assets/images/foodie.jpg",
+  imageCaption: "Today you may expect some delicious food! No matter if it's either healthy snack, little salami or big dinner, it definitely will fill you up with happiness."
+};
+
+deck[13] = {
+  image13: new Image(),
+  src: "assets/images/fragile.jpg",
+  imageCaption: "You may feel very fragile- either physically, emotionally or mentally. Take it easy today, and don't be afraid to share your feelings with your close ones!"
+};
+
+deck[14] = {
+  image14: new Image(),
+  src: "assets/images/friend-hangout.jpg",
+  imageCaption: "Spend your time with your friends today. Even if it's going to be a short hangout it will brighten your day and you will have a great time together!"
+};
+
+deck[15] = {
+  image15: new Image(),
+  src: "assets/images/fully-charged.jpg",
+  imageCaption: "You are going to feel full of energy. This day is going to be full of positive vibes!"
+};
+
+deck[16] = {
+  image16: new Image(),
+  src: "assets/images/guests-over.jpg",
+  imageCaption: "You are going to be busy with guests today. If you didn't expect any- now you know that you may be surprised by unanounced visit!"
+};
+
+deck[17] = {
+  image17: new Image(),
+  src: "assets/images/job-opportunity.jpg",
+  imageCaption: "New job opportunity may arise within your area of interest! Be ready to update your CV as it may be your chance for a pay raise!"
+};
+
+deck[18] = {
+  image18: new Image(),
+  src: "assets/images/lazy-cosy.jpg",
+  imageCaption: "Today maybe you may not do much, but don't worry. Taking a day off from your tasks may help your brain to recover and become more productive tomorrow!"
+};
+
+deck[19] = {
+  image19: new Image(),
+  src: "assets/images/love-messenger.jpg",
+  imageCaption: "Love has many faces. Whichever of them you choose today, make sure to value and celebrate this feeling!"
+};
+
+deck[20] = {
+  image20: new Image(),
+  src: "assets/images/nice.jpg",
+  imageCaption: "Having THAT outfit on surely feels good. Today you will get an extra confidence boost thanks to that."
+};
 
 // Prints the array of cards to the console
 
-for (x = 0; x < deck.length; x++) {
-  console.log(deck[x].description);
+for(x = 0; x < deck.length; x++) {
+  console.log(deck[x].src);
 }
 
 /**
@@ -113,32 +197,40 @@ function pageThree() {
 }
 
 /**
- * Function randomizing the content of 2 different sets of card arrays- in progress
+ * Function randomizing the content of 2 different sets of card arrays- in progress (needs FIX!!!)
  */
 
 function setRandomize() {
 
-  let deckShuffle;
-  // Loops through the deck arrays - in progress
-  for (let i = 0; i = deck.length; i++) {
-    deckShuffle = Math.floor(Math.random() * (deck.length));
-    return deckShuffle;
-  }
+    let cardSet1 = [];
+    let cardSet2 = [];
 
-  // Pushes the results into 2 sets
-  let cardSet1 = [];
-  let cardSet2 = [];
+    for (let i = 0; i <= deck.length; i++) {
 
-  // Throws undefined, to be fixed
-  cardSet1.push(deckShuffle);
-  cardSet2.push(deckShuffle);
-  
+      let deckA = deck;
+
+      let shuffle = Math.floor(Math.random() * (deck.length));
+      
+      // three buckets shuffle method
+      let tempCard = deckA[i];
+      deckA[i] = deckA[shuffle];
+      deckA[shuffle] = tempCard;
+      
+
+      console.log(deckA, "after shuffle"); // reshuffled
+    
+
+    let shuffled1= deckA.slice(0, deck.length / 2);
+    let shuffled2 = deckA.slice(0, deck.length / 2, deck.length);
+    
+    cardSet1.push(shuffled1[i]);
+    cardSet2.push(shuffled2[i]);
+
+  };
   // check
   console.log(cardSet1);
   console.log(cardSet2);
-
-}
-
+};
 
 /**
  * On click "card shuffling" function that draws 3 random cards out of the arrays from the set
